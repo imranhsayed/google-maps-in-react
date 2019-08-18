2b67deabc8448a5dd4b58e266cd267a61951f020
Imran Sayed <imranhsayed@gmail.com>
2019-08-18 17:57:51 +0530

    Update readme

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, withScriptjs, InfoWindow, Marker } from "react-google-maps";
 import Geocode from "react-geocode";
 import Autocomplete from 'react-google-autocomplete';
-Geocode.setApiKey( "AIzaSyDgi0yfFwjMIqw3BAnjBDETa68ra3ZGzOk" );
+Geocode.setApiKey( "xxxxxx" );
 Geocode.enableDebug();
 
 class Map extends Component{
